c973c61f98026d13ad5294dafe36746d63eef26c
jithin <jithinp69@gmail.com>
2022-09-24 20:58:17 +0530

    multithreadig : by extending class and implementing Runnable

---
+++ Unacademy/MultiThreading/MultiThreading/ExtendingThread.java
@@ -0,0 +1,13 @@
+package MultiThreading;
+
+public class ExtendingThread extends Thread {
+    
+    public void run(){  // Running State
+        System.out.println("Thread is running");
+    }
+
+    public static void main(String[] args) {
+        ExtendingThread t1 = new ExtendingThread();  // object of class Main, as we extending to Thread it will become Thread.
+        t1.start();   // Runnable state.
+    }
+}
---
+++ Unacademy/MultiThreading/implementingRunnable.java
@@ -0,0 +1,13 @@
+
+
+public class implementingRunnable implements Runnable {
+    public void run(){
+        System.out.println("Thread running in Runnable Interface");
+    }
+
+    public static void main(String[] args) {
+        implementingRunnable ir1 = new implementingRunnable();    // object
+        Thread t1 = new Thread(ir1);  
+        t1.start(); 
+    }
+}
